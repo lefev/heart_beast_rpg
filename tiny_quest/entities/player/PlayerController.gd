13eaed1a76c7e604a2c4dd68ad2566094ba32831
lefev <pascal.lefev@gmail.com>
2021-04-26 19:39:55 +0200

    feat: adds attack animations and introduces a simple state machine

--- tiny_quest/entities/player/PlayerController.gd
+++ tiny_quest/entities/player/PlayerController.gd
@@ -6,28 +6,54 @@ const MAX_SPEED : float = 80.0
 const ACCELERATION : float = 600.0
 const FRICTION : float = 600.0
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
 # vars
+var current_state = MOVE 
 var velocity : Vector2 = Vector2.ZERO
 
 onready var animation_player : AnimationPlayer = $AnimationPlayer
 onready var animation_tree : AnimationTree = $AnimationTree
 onready var animation_state = animation_tree.get("parameters/playback")
 
-# methods
+
+func _ready()  -> void:
+	animation_tree.active = true
+
+
+func _physics_process(delta: float) -> void:
+	match current_state:
+		MOVE:
+			move_state(delta)
+			
+		ROLL:
+			pass
+			
+		ATTACK:
+			attack_state(delta)
+			pass
+	pass
+
+
 func _get_movement_input() -> Vector2:
 	var movement_input : Vector2 = Vector2.ZERO
 	movement_input.x = Input.get_action_strength('move_right') - Input.get_action_strength('move_left')
 	movement_input.y = Input.get_action_strength('move_down') - Input.get_action_strength('move_up')
 	movement_input = movement_input.normalized()
 	return movement_input
-	
-# processes
-func _physics_process(delta: float) -> void:
+
+
+func move_state(delta : float) -> void:
 	var movement_input : Vector2 = _get_movement_input()
 	
 	if movement_input != Vector2.ZERO:
 		animation_tree.set("parameters/idle/blend_position", movement_input)
 		animation_tree.set("parameters/run/blend_position", movement_input)
+		animation_tree.set("parameters/attack/blend_position", movement_input)
 		animation_state.travel("run")
 		velocity = velocity.move_toward(movement_input * MAX_SPEED, ACCELERATION * delta)
 	else:
@@ -35,4 +61,16 @@ func _physics_process(delta: float) -> void:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("attack"):
+		current_state = ATTACK
+	pass
 
+func attack_state(_delta : float) -> void:
+	# Stop sliding while attacking
+	velocity = Vector2.ZERO
+	animation_state.travel("attack")
+	pass
+
+func attack_animation_finished() -> void:
+	current_state = MOVE
