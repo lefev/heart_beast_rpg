1da9c90aadfa20cc59542e4fabd069ebf7d55904
lefev <pascal.lefev@protonmail.com>
2021-05-08 13:50:58 +0200

    feat: adds rolling state and animation to the player

--- tiny_quest/entities/player/PlayerController.gd
+++ tiny_quest/entities/player/PlayerController.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 # Constants
 # Movement
 const MAX_SPEED : float = 80.0
+const ROLL_SPEED : float = MAX_SPEED * 1.4
 const ACCELERATION : float = 600.0
 const FRICTION : float = 600.0
 
@@ -15,6 +16,7 @@ enum {
 # vars
 var current_state = MOVE 
 var velocity : Vector2 = Vector2.ZERO
+var velocity_roll : Vector2 = Vector2.LEFT
 
 onready var animation_player : AnimationPlayer = $AnimationPlayer
 onready var animation_tree : AnimationTree = $AnimationTree
@@ -30,12 +32,9 @@ func _physics_process(delta: float) -> void:
 		MOVE:
 			move_state(delta)
 		ROLL:
-			pass
-			
+			roll_state(delta)
 		ATTACK:
 			attack_state(delta)
-			pass
-	pass
 
 
 func _get_movement_input() -> Vector2:
@@ -50,28 +49,47 @@ func move_state(delta : float) -> void:
 	var movement_input : Vector2 = _get_movement_input()
 	
 	if movement_input != Vector2.ZERO:
+		velocity_roll = movement_input
 		animation_tree.set("parameters/idle/blend_position", movement_input)
 		animation_tree.set("parameters/run/blend_position", movement_input)
 		animation_tree.set("parameters/attack/blend_position", movement_input)
+		animation_tree.set("parameters/roll/blend_position", movement_input)
 		animation_state.travel("run")
 		velocity = velocity.move_toward(movement_input * MAX_SPEED, ACCELERATION * delta)
 	else:
 		animation_state.travel("idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	velocity = move_and_slide(velocity)
+	move()
+	
+	if Input.is_action_just_pressed("roll"):
+		current_state = ROLL
 	
 	if Input.is_action_just_pressed("attack"):
 		current_state = ATTACK
-	pass
 
+
+func roll_animation_finished() -> void:
+	current_state = MOVE
+
+
+func roll_state(_delta : float) -> void:
+	velocity = velocity_roll * ROLL_SPEED
+	animation_state.travel("roll")
+	move()
+	
 
 func attack_state(_delta : float) -> void:
 	# Stop sliding while attacking
 	velocity = Vector2.ZERO
 	animation_state.travel("attack")
-	pass
-
+	
 
 func attack_animation_finished() -> void:
 	current_state = MOVE
+	# magic number x.x
+	velocity = velocity * 0.8
+	
+
+func move() -> void:
+	velocity = move_and_slide(velocity)
