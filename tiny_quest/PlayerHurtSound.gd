18c1760378eb6e83c197f1e8ffc354d66a6c9977
lefev <pascal.lefev@protonmail.com>
2021-05-12 10:25:55 +0200

    fix: uses the connect return value

--- tiny_quest/PlayerHurtSound.gd
+++ tiny_quest/PlayerHurtSound.gd
@@ -1,4 +1,6 @@
 extends AudioStreamPlayer
 
 func _ready() -> void:
-	connect("finished", self, "queue_free")
+	# clears the audio player even if the finished signal cannot be connected
+	if connect("finished", self, "queue_free") != 0:
+		queue_free()
